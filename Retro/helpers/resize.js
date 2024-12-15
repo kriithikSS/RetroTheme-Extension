d8b8bf4ad7b4cb059d18ff939f7b56f5be0d01dc
Kriithik SS <160462509+kriithikSS@users.noreply.github.com>
2024-12-15 21:23:54 +0530

    Delete Retro directory

--- Retro/helpers/resize.js
+++
@@ -1,4 +0,0 @@
-var player = document.querySelector('#movie_player');
-var width = document.body.getAttribute('redux-player-width');
-var height = document.body.getAttribute('redux-player-height');
-player.setInternalSize(width, height);
